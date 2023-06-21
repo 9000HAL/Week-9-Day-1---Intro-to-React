a220b6d0c7affe5d7f3f6bcf5fe3a8abcfad4cfc
9000HAL <gsanchezscs@gmail.com>
2023-06-21 05:26:57 -0700

    5/30am save

---
+++ app/src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+
+const NavBar = () => {
+    return (
+        <>
+            <h1>NavBar</h1>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                            Bootstrap
+                    </a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import NavBar from './components/NavBar'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App/>
+    <NavBar/>
   </React.StrictMode>,
 )
