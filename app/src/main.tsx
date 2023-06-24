66f63a4bc183577b2f31807c21f5362229471669
9000HAL <gsanchezscs@gmail.com>
2023-06-24 01:48:10 -0700

    revisingg commit

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar' //YESSSSSSSS NO ERRORS
+// NOOOOOOOO: import NavBar from 'app/src/components/NavBar.tsx'
+//import NavBar from '/Users/gabesanchez/Desktop/Week-9-Day-1---Intro-to-React/app/src/components/NavBar.tsx' //YESSSSSSS WITH ERROR MESSAGE
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 
@@ -11,3 +13,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <NavBar/>
   </React.StrictMode>,
 )
+
+
+
+//  /Users/gabesanchez/Desktop/Week-9-Day-1---Intro-to-React/app/src/components/NavBar.tsx
+
+// app/src/components/NavBar.tsx
